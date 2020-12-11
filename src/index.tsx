b76f21319013f01571065e63f788e49fb606e8c7
celinedesnoe <cdesnoe@meilleursagents.com>
2020-12-11 17:57:50 +0100

    fix modules

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import rootReducer from "./reducers/rootReducer";
+import { createStore } from "redux";
+
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
